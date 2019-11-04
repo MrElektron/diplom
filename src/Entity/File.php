ae11029160930ca64aeeb2ae2e86c63a5ddf0820
MrElektron <kukhtevin@yandex.ru>
2019-11-04 22:08:45 +0500

    Upload file functional

---
+++ src/Entity/File.php
@@ -0,0 +1,234 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * File
+ *
+ * @ORM\Table(name="file")
+ * @ORM\Entity(repositoryClass="App\Repository\FileRepository")
+ */
+class File
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="file_size", type="string", length=255)
+     */
+    private $fileSize;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="format", type="string", length=255)
+     */
+    private $format;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="uploaded_at", type="datetime")
+     */
+    private $uploadedAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="file_name", type="text")
+     */
+    private $fileName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="stored_file_name", type="text")
+     */
+    private $storedFileName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="stored_preview_file_name", type="text", nullable=true)
+     */
+    private $storedPreviewFileName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="stored_file_dir", type="text", nullable=true)
+     */
+    private $storedFileDir;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $fileSize
+     *
+     * @return $this
+     */
+    public function setFileSize($fileSize)
+    {
+        $this->fileSize = $fileSize;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getFileSize()
+    {
+        return $this->fileSize;
+    }
+
+    /**
+     * Set format
+     *
+     * @param string $format
+     *
+     * @return $this
+     */
+    public function setFormat($format)
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+
+    /**
+     * Get format
+     *
+     * @return string
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    /**
+     * Set uploadedAt
+     *
+     * @param \DateTime $uploadedAt
+     *
+     * @return $this
+     */
+    public function setUploadedAt($uploadedAt)
+    {
+        $this->uploadedAt = $uploadedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get uploadedAt
+     *
+     * @return \DateTime
+     */
+    public function getUploadedAt()
+    {
+        return $this->uploadedAt;
+    }
+
+    /**
+     * Set fileName
+     *
+     * @param string $fileName
+     *
+     * @return $this
+     */
+    public function setFileName($fileName)
+    {
+        $this->fileName = $fileName;
+
+        return $this;
+    }
+
+    /**
+     * Get fileName
+     *
+     * @return string
+     */
+    public function getFileName()
+    {
+        return $this->fileName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStoredFileName()
+    {
+        return $this->storedFileName ?: $this->fileName;
+    }
+
+    /**
+     * @param string $storedFileName
+     * @return $this
+     */
+    public function setStoredFileName($storedFileName)
+    {
+        $this->storedFileName = $storedFileName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStoredPreviewFileName()
+    {
+        return $this->storedPreviewFileName ?: $this->fileName;
+    }
+
+    /**
+     * @param string $storedPreviewFileName
+     * @return $this
+     */
+    public function setStoredPreviewFileName($storedPreviewFileName)
+    {
+        $this->storedPreviewFileName = $storedPreviewFileName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStoredFileDir()
+    {
+        return $this->storedFileDir;
+    }
+
+    /**
+     * @param string $storedFileDir
+     * @return File
+     */
+    public function setStoredFileDir($storedFileDir)
+    {
+        $this->storedFileDir = $storedFileDir;
+        return $this;
+    }
+}
+
